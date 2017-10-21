5e3cb9a1d94655cea3814b132758e42468175a45
jy <proghjy@gmail.com>
2017-10-21 15:33:05 +0900

    #2 변수 및 숫자 학습

---
+++ PyRecipe.py
@@ -0,0 +1,13 @@
+a = 7
+print(a) # 7
+b = a
+print(b) # 7
+print(type(a)) # <class 'int'>
+print(type(b)) # <class 'int'>
+print(type(58)) # <class 'int'>
+print(type(99.9)) # <class 'float'>
+print(type('abc')) # <class 'str'>
+
+googol = 10**100
+print(googol)
+print(googol * googol)
