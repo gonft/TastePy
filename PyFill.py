d4e8d2d62370e0e06013de890f9215f9e0beb9c2
jy <proghjy@gmail.com>
2017-10-21 19:22:33 +0900

    #3 test list

--- PyFill.py
+++ PyFill.py
@@ -20,3 +20,32 @@ splitme = 'a/b//c/d///e'
 print(splitme.split('/'))   #['a', 'b', '', 'c', 'd', '', '', 'e']
 splitme = 'a/b//c/d///e'
 print(splitme.split('//'))  #['a/b', 'c/d', '/e']
+
+marxes = ['Groucho', 'Chico', 'Harpo']
+print(marxes[0])    #Groucho
+print(marxes[1])    #Chico
+print(marxes[2])    #Harpo
+
+print(marxes[-1])    #Harpo
+print(marxes[-2])    #Chico
+print(marxes[-3])    #Groucho
+
+marxes.append('Zeppo')
+print(marxes)   #['Groucho', 'Chico', 'Harpo', 'Zeppo']
+others = ['Gummo', 'Karl']
+marxes.extend(others) #or marex += other
+print(marxes)   #['Groucho', 'Chico', 'Harpo', 'Zeppo', 'Gummo', 'Karl']
+
+del marxes[-2]  #or marxes.remove('Gummo')
+print(marxes)   #['Groucho', 'Chico', 'Harpo', 'Zeppo']
+marxes.insert(-1,'Gummo')
+print(marxes)   #['Groucho', 'Chico', 'Harpo', 'Zeppo', 'Gummo', 'Karl']
+marxes.pop()    #equal marxes.pop(-1)
+print(marxes)   #['Groucho', 'Chico', 'Harpo', 'Zeppo', 'Gummo']
+print(marxes.index('Harpo'))    #2
+print(', '.join(marxes))    #Groucho, Chico, Harpo, Zeppo, Gummo
+
+a = [1, 2, 3]
+b = a.copy(a)
+c = list(a)
+d = a[:]
