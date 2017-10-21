80630779600543336209700a62baf8f0291c90a4
jy <proghjy@gmail.com>
2017-10-21 18:25:29 +0900

    #3 split()

--- PyFill.py
+++ PyFill.py
@@ -9,3 +9,14 @@ print(weekdays)         #['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'
 print(big_birds)        #['emu', 'ostrich', 'cassowary']
 print(first_names)      #['Graham', 'Jonh', 'Terry', 'Terry', 'Michael']
 print(list('cat'))      #['c', 'a', 't']
+
+a_tuple = ('ready', 'fire', 'aim')
+print(list(a_tuple))    #['ready', 'fire', 'aim']
+
+birthday = '1/6/1952'
+print(birthday.split('/'))  #['1', '6', '1952']
+
+splitme = 'a/b//c/d///e'
+print(splitme.split('/'))   #['a', 'b', '', 'c', 'd', '', '', 'e']
+splitme = 'a/b//c/d///e'
+print(splitme.split('//'))  #['a/b', 'c/d', '/e']
