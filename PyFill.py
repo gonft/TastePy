2a62b74bb889cb5fd88be352f83e1491f41e202c
jy <proghjy@gmail.com>
2017-10-21 16:23:19 +0900

    #3 List

---
+++ PyFill.py
@@ -0,0 +1,11 @@
+empty_list = []
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+big_birds = ['emu', 'ostrich', 'cassowary']
+first_names = ['Graham', 'Jonh', 'Terry', 'Terry', 'Michael']
+
+another_empty_list = list()
+
+print(weekdays)         #['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+print(big_birds)        #['emu', 'ostrich', 'cassowary']
+print(first_names)      #['Graham', 'Jonh', 'Terry', 'Terry', 'Michael']
+print(list('cat'))      #['c', 'a', 't']
