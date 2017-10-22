e3d1a06495ad12a08e9d25ac782ca2ba0dd510cf
jy <proghjy@gmail.com>
2017-10-22 12:29:27 +0900

    #3 Tuple

--- PyFill.py
+++ PyFill.py
@@ -1,51 +1,17 @@
-empty_list = []
-weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-big_birds = ['emu', 'ostrich', 'cassowary']
-first_names = ['Graham', 'Jonh', 'Terry', 'Terry', 'Michael']
+one_marx = 'Groucho',
+print(one_marx) #('Groucho',)
 
-another_empty_list = list()
+marx_tuple = ('Groucho', 'Chico', 'Harpo') #'Groucho', 'Chico', 'Harpo'
+print(marx_tuple)   #('Groucho', 'Chico', 'Harpo')
 
-print(weekdays)         #['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-print(big_birds)        #['emu', 'ostrich', 'cassowary']
-print(first_names)      #['Graham', 'Jonh', 'Terry', 'Terry', 'Michael']
-print(list('cat'))      #['c', 'a', 't']
+# tuple unpacking
+a, b, c = marx_tuple
+print(a, b, c)  #Groucho Chico Harpo
 
-a_tuple = ('ready', 'fire', 'aim')
-print(list(a_tuple))    #['ready', 'fire', 'aim']
+password = 'swordfish'
+icecream = 'tuttifrutti'
+password, icecream = icecream, password
+print(password, icecream)   #tuttifrutti swordfish
 
-birthday = '1/6/1952'
-print(birthday.split('/'))  #['1', '6', '1952']
-
-splitme = 'a/b//c/d///e'
-print(splitme.split('/'))   #['a', 'b', '', 'c', 'd', '', '', 'e']
-splitme = 'a/b//c/d///e'
-print(splitme.split('//'))  #['a/b', 'c/d', '/e']
-
-marxes = ['Groucho', 'Chico', 'Harpo']
-print(marxes[0])    #Groucho
-print(marxes[1])    #Chico
-print(marxes[2])    #Harpo
-
-print(marxes[-1])    #Harpo
-print(marxes[-2])    #Chico
-print(marxes[-3])    #Groucho
-
-marxes.append('Zeppo')
-print(marxes)   #['Groucho', 'Chico', 'Harpo', 'Zeppo']
-others = ['Gummo', 'Karl']
-marxes.extend(others) #or marex += other
-print(marxes)   #['Groucho', 'Chico', 'Harpo', 'Zeppo', 'Gummo', 'Karl']
-
-del marxes[-2]  #or marxes.remove('Gummo')
-print(marxes)   #['Groucho', 'Chico', 'Harpo', 'Zeppo']
-marxes.insert(-1,'Gummo')
-print(marxes)   #['Groucho', 'Chico', 'Harpo', 'Zeppo', 'Gummo', 'Karl']
-marxes.pop()    #equal marxes.pop(-1)
-print(marxes)   #['Groucho', 'Chico', 'Harpo', 'Zeppo', 'Gummo']
-print(marxes.index('Harpo'))    #2
-print(', '.join(marxes))    #Groucho, Chico, Harpo, Zeppo, Gummo
-
-a = [1, 2, 3]
-b = a.copy(a)
-c = list(a)
-d = a[:]
+marx_list = ['Groucho', 'Chico', 'Harpo']
+print(tuple(marx_list)) #('Groucho', 'Chico', 'Harpo')
