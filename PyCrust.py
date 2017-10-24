94631a15d5fe8145572918300f7c40aa0c86881e
jy <proghjy@gmail.com>
2017-10-24 23:49:45 +0900

    Compare with if, elif, and else #4

--- PyCrust.py
+++ PyCrust.py
@@ -1,16 +1,52 @@
-alphabet = ''
-alphabet += 'abcdefg'
-alphabet += 'hijklmnop'
-alphabet += 'qrstuv'
-alphabet += 'wxyz'
-print(alphabet) #abcdefghijklmnopqrstuvwxyz
+disaster = True
+if disaster:
+    print("Woe!")
+else:
+    print("Whee!")
+#Woe!
 
-alphabet = 'abcdefg' + \
-           'hijklmnop' + \
-           'qrstuv' + \
-           'wxyz'
-print(alphabet) #abcdefghijklmnopqrstuvwxyz
+furry = True
+small = True
+if furry:
+    if small:
+        print("It's a cat.")
+    else:
+        print("It's a bear!")
+else:
+    if small:
+        print("It's a skink!")
+    else:
+        print("It's a human. Or a hairless bear.")
+#It's a cat.
 
-sum = 1 + 2 + \
-    3
-print(sum)      #6
+color = 'puce'
+if color == "red":
+    print("It's a tomato")
+elif color == "grren":
+    print("It's a green pepper")
+elif color == "bee purple":
+    print("I don't know what it is, but only bees can see it")
+else:
+    print("I've never heard of the color", color)
+#I've never heard of the color puce
+
+x = 7
+print(x == 5)                   #False
+print(x == 7)                   #True
+print(5 < x)                    #True
+print(x < 10)                   #True
+print(5 < x and x < 10)         #True
+print((5 < x) and (x < 10))     #True
+print(5 < x or x < 10)          #True
+print(5 < x and x > 10)         #False
+print(5 < x and not x > 10)     #True
+
+print(5 < x < 10)               #True
+print(5 < x < 10 < 999)         #True
+
+some_list = []
+if some_list:
+    print("There's something in here")
+else:
+    print("Hey, it's empty!")
+#Hey, it's empty!
