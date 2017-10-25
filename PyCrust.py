ae385528f5fea19b010efe31d83ec08fc9049445
jy <proghjy@gmail.com>
2017-10-25 22:41:24 +0900

    Repeat with while #4

--- PyCrust.py
+++ PyCrust.py
@@ -9,11 +9,21 @@ while count <= 5:
 # 5
 
 while True:
-    stuff = input("String to capitalize [type q to quit]: ")
-    if stuff == "q":
+    value = input("String to capitalize [type q to quit]: ")
+    if value == "q":    # q 종료
         break
-    print(stuff.capitalize())
+    number = int(value)
+    if number % 2 == 0: # 짝수
+        continue
+    print(number, "squared is", number * number)
+# String to capitalize [type q to quit]: 1
+# 1 squared is 1
+# String to capitalize [type q to quit]: 2
+# String to capitalize [type q to quit]: 3
+# 3 squared is 9
+# String to capitalize [type q to quit]: 4
+# String to capitalize [type q to quit]: 5
+# 5 squared is 25
 # String to capitalize [type q to quit]: q
 #
 # Process finished with exit code 0
-
