ce7302e6519a2a77a228187859810a7bb0702daf
proghjy <proghjy@gmail.com>
2019-04-14 20:18:21 +0900

    #4 Functions - Handle Errors with try and except

--- PyCrust.py
+++ PyCrust.py
@@ -253,4 +253,14 @@ def change_local():
 
 print(animal)
 change_local()
-print('global', globals())
+# print('global', globals())
+
+# Handle Errors with try and except
+
+short_list = [1, 2, 3]
+position = 5
+try:
+    short_list[position]
+except:
+    print('Need a position between 0 and', len(
+        short_list)-1, ' but got', position)
