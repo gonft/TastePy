2508bd4873516e529cd4680a911c1c4984b83026
proghjy <proghjy@gmail.com>
2019-04-14 19:25:15 +0900

    Functions #4

--- PyCrust.py
+++ PyCrust.py
@@ -1,21 +1,30 @@
 # Functions
 
 # Specify Default Parameter Values
+
+
 def menu(wine, entree, dessert='pudding'):
     return {'wine': wine, 'entree': entree, 'dessert': dessert}
 
+
 print(menu('chardonnay', 'chicken'))
 # {'wine': 'chardonnay', 'entree': 'chicken', 'dessert': 'pudding'}
+
+
 def buggy(arg, result=[]):
     result.append(arg)
     print(result)
+
 
 buggy('a')  # ['a']
 buggy('b')  # ['a', 'b']
 
 # Gather Positional Arguments with *
+
+
 def print_args(*args):
     print('Positional argument tuple:', args)
+
 
 print_args()
 # Positional argument tuple: ()
@@ -23,27 +32,33 @@ print_args()
 print_args(3, 2, 1, 'wait!', 'uh...')
 # Positional argument tuple: (3, 2, 1, 'wait!', 'uh...')
 
+
 def print_more(required1, required2, *args):
     print('Need this one:', required1)
     print('Nedd this one too:', required2)
     print('All the rest:', args)
+
 
 print_more('cap', 'gloves', 'scarf', 'monocle', 'mustanche wa')
 # All the rest: ('scarf', 'monocle', 'mustanche wa')
 
 # Gather Positional Arguments with **
 
+
 def print_kwargs(**kwargs):
     print('Keyword arguments:', kwargs)
+
 
 print_kwargs(wine='merlot', entree='mutton', dessert='macaroon')
 # Keyword arguments: {'wine': 'merlot', 'entree': 'mutton', 'dessert': 'macaroon'}
 
 # Docstrings
 
+
 def echo(anythings):
     'echo returns its input argument'
     return anythings
+
 
 def print_if_ture(thing, check):
     '''
@@ -55,5 +70,96 @@ def print_if_ture(thing, check):
     if check:
         print(thing)
 
+
 help(echo.__doc__)
 # 'echo returns its input argument'.
+
+# Functions Are First-Class Citizens
+
+
+def answer():
+    print(42)
+
+
+answer()
+
+
+def run_something(func):
+    func()
+
+
+run_something(answer)
+
+print(type(run_something))
+
+
+def add_args(arg1, arg2):
+    print(arg1 + arg2)
+
+
+print(type(add_args))
+
+
+def run_somthing_with_args(func, arg1, arg2):
+    func(arg1, arg2)
+
+
+run_somthing_with_args(add_args, 5, 9)
+
+
+def sum_args(*args):
+    return sum(args)
+
+
+def run_with_positional_args(func, *args):
+    return func(*args)
+
+
+print(run_with_positional_args(sum_args, 1, 2, 3, 4))
+
+# Inner Functions
+
+
+def outer(a, b):
+    def inner(c, d):
+        return c + d
+    return inner(a, b)
+
+
+print(outer(4, 7))
+
+# Closures
+
+
+def knight2(saying):
+    def inner2():
+        return "We are the knight who say: '%s'" % saying
+    return inner2
+
+
+a = knight2('Duck')
+b = knight2('Hasenpfeffer')
+
+print(type(a))
+print(type(b))
+
+print(a())
+print(b())
+
+# Anonymous Functions: the lambda() Function
+
+
+def edit_story(words, func):
+    for word in words:
+        print(func(word))
+
+
+stairs = ['thud', 'meow', 'thud', 'hiss']
+
+
+def enliven(word):
+    return word.capitalize() + '!'
+
+
+# edit_story(stairs, enliven)
+edit_story(stairs, lambda word: word.capitalize() + '!')
